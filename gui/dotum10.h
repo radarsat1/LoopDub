6d907051fbb360b6d0d67a37a7638965fc30916b
radarsat1 <radarsat1>
2006-06-24 16:03:26 +0000

    The font is now embedded in the executable instead of loaded at run-time.

---
+++ gui/dotum10.h
@@ -0,0 +1,20 @@
+
+#ifndef _FONTDATA_H_
+#define _FONTDATA_H_
+
+extern
+unsigned short Font_CharOffsets[95];
+
+extern
+unsigned char Font_CharWidths[95];
+
+extern
+unsigned int Font_BitmapWidth;
+
+extern
+unsigned int Font_Height;
+
+extern
+unsigned char Font_CharacterData[6897];
+
+#endif // _FONTDATA_H_

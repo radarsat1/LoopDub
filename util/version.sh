1c14fc5b449df5e5a4464eb3354b677a87eff4af
Stephen Sinclair <radarsat1@gmail.com>
2009-11-06 10:06:04 -0500

    Fix version script by changing "git-<cmd>" to "git <cmd>"

--- util/version.sh
+++ util/version.sh
@@ -4,14 +4,14 @@
 # based on the number of commits since the last-tagged version, the
 # current branch, and any uncommitted changes.
 
-BRANCH="$(git-branch | grep \* | sed s,/,_,g | awk '{print $2}')"
+BRANCH="$(git branch | grep \* | sed s,/,_,g | awk '{print $2}')"
 
-LASTTAG="$(git-tag -l | perl -nle 'print $_  if m/\d+\.\d+(\.\d+)*/' | head -n 1)"
+LASTTAG="$(git tag -l | perl -nle 'print $_  if m/\d+\.\d+(\.\d+)*/' | head -n 1)"
 if [ "$LASTTAG"x = x ]; then
     LASTTAG="0"
     INCREMENT="-0"
 else
-    INCREMENT=-$(git-rev-list "$LASTTAG"..HEAD | wc -l | awk '{print $1}')
+    INCREMENT=-$(git rev-list "$LASTTAG"..HEAD | wc -l | awk '{print $1}')
     if [ "$INCREMENT"x = "-0" ]; then
         INCREMENT=""
     fi
@@ -23,7 +23,7 @@ if [ "$BRANCH"x = -masterx ]; then
 fi
 
 DIFF=""
-if [ "$(git-diff | head -n 5)"x != x ]; then
+if [ "$(git diff | head -n 5)"x != x ]; then
 
     # determine checksum program to use
     SUM=$(which sha1sum | awk '{print $1}')
@@ -38,7 +38,7 @@ if [ "$(git-diff | head -n 5)"x != x ]; then
     if [ "$SUM"x = x -o "$SUM" = nox ]; then
         DIFF="-dirty"
     else
-        DIFF=-"$(git-diff | $SUM | awk '{print substr($1,0,8)}')"
+        DIFF=-"$(git diff | $SUM | awk '{print substr($1,0,8)}')"
     fi
 
 fi
